74e0f06b5ed6cb2c939efb24194514ac6ad94a40
Yohann Mansiaux <y.mansiaux@epiconcept.fr>
2025-07-04 15:17:27 +0200

    test: setup testthat

---
+++ tests/testthat.R
@@ -0,0 +1,10 @@
+# This file is part of the standard setup for testthat.
+# It is recommended that you do not modify it.
+#
+# Where is this file located? See the docs for online
+# documentation, or run `usethis::use_testthat()` to re-create it.
+
+library(testthat)
+library(blueskyR)
+
+test_check("blueskyR")
