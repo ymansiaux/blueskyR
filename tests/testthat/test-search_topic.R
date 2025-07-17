f938a7122355b38da469f3b4cf6fcfa0dcac9553
Yohann Mansiaux <y.mansiaux@epiconcept.fr>
2025-07-17 09:43:10 +0200

    test: add extra test for search_topic when the query has no results

--- tests/testthat/test-search_topic.R
+++ tests/testthat/test-search_topic.R
@@ -43,3 +43,39 @@ test_that("search_topic works", {
 })
 
 unlink(list.files(conf$data_dir, full.names = TRUE, recursive = TRUE))
+
+
+test_that("search_topic works with a fake topic", {
+  skip_on_ci()
+  plan <- list(
+    research_max_date = "2025-07-15T00:00:00",
+    research_min_date = "2025-07-10T00:00:00",
+    requests = 0
+  )
+
+  has_more <- TRUE
+  random_topic <- paste0(sample(LETTERS, 100, replace = TRUE), collapse = "")
+  expect_no_error(
+    while (has_more) {
+      plan <- search_topic(
+        plan,
+        random_topic,
+        random_topic,
+        output_in_scala = FALSE
+      )
+      has_more <- plan$has_more
+    }
+  )
+  expect_equal(plan$requests, 1)
+
+  all_files <- list.files(
+    conf$data_dir,
+    full.names = TRUE,
+    recursive = TRUE
+  )
+  json_files <- all_files[grepl("^\\d{4}.*\\.json$", basename(all_files))]
+
+  expect_equal(length(json_files), 0)
+})
+
+unlink(list.files(conf$data_dir, full.names = TRUE, recursive = TRUE))
